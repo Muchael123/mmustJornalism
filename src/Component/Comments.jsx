fb835247f74ca0261007c54cfe14627b62fcc69a
Byrone Kinsly <kinslybyrone17@gmail.com>
2023-11-22 12:31:38 +0300

    Updated Comments Section

--- src/Component/Comments.jsx
+++ src/Component/Comments.jsx
@@ -39,22 +39,15 @@ const Comments = ({category,image_id}) => {
     };
 
     return (
-        <div className='ml-8'>
-          <h1 className='w-1/3 uppercase font-bold text-xl underline decoration-orange-500 decoration-4 my-5 hover:no-underline'>LEAVE A COMMENT</h1>
+        <div className='ml-32'>
+          <h1 className='w-1/3 uppercase font-bold text-xl  my-5 '>LEAVE A COMMENT</h1>
             <label>
                 {/* Comment: */}
-                <textarea id='textarea' name='textarea' rows="4" cols="50" placeholder='Your Comment' className='pl-2 resize border-2' value={content} onChange={(e) => setContent(e.target.value)} />
+                <textarea id='textarea' name='textarea' rows="4" cols="50" placeholder='Your Comment' className='pl-2  resize-none border-2' value={content} onChange={(e) => setContent(e.target.value)} />
             </label>
             <br />
-            {/* <label>
-                Is Anonymous:
-                <input
-                    type="checkbox"
-                    checked={isAnonymous}
-                    onChange={() => setIsAnonymous(!isAnonymous)}
-                />
-            </label> */}
-            <button onClick={handleSubmit} className=' my-2 px-4 py-2 bg-black text-white text-lg cursor-pointer hover:bg-orange-500 uppercase'>Submit</button>
+            
+            <button onClick={handleSubmit} className=' my-2 px-4 py-2 bg-gradient-to-r from-indigo-500 via-purple-700 to-pink-500 text-white text-lg cursor-pointer hover:opacity-70 uppercase'>Submit</button>
         </div>
     );
 };
