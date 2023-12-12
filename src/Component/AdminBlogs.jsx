8f0a81d65084f72a999cf7aa5efe2e5fb9931e43
David-mwas <dmwas704@gmail.com>
2023-12-12 18:48:57 +0300

    Remove :Remove background red on edit icon

--- src/Component/AdminBlogs.jsx
+++ src/Component/AdminBlogs.jsx
@@ -90,7 +90,7 @@ function AdminBlogs(props) {
                 </svg>
               </a>
             </li>
-            <li className="bg-red-500 cursor-pointer" onClick={editBlog}>
+            <li className=" cursor-pointer" onClick={editBlog}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
