8f249dec9d8509bdd0c7cb1f300b36db843c3d4f
David-mwas <dmwas704@gmail.com>
2023-12-12 16:52:52 +0300

    add :ad required={true}, add editblog

--- src/Pages/EditBlog.jsx
+++ src/Pages/EditBlog.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import AdminNavbar from "../Component/AdminPanel/AdminNavbar";
+import AdminSidebar from "../Component/AdminSidebar";
+import Login from "../Pages/Login";
+import Dashboard from "../Component/AdminPanel/Dashboard";
+import CreateBlog from "./CreateBlog";
+import ProfilePage from "../Component/AdminPanel/ProfilePage";
+import AdminNews from "../Component/AdminPanel/AdminNews";
+import AdminBusiness from "../Component/AdminPanel/AdminBusiness";
+import AdminEntertainment from "../Component/AdminPanel/AdminEntertainment";
+import AdminSports from "../Component/AdminPanel/AdminSports";
+import BlogEdit from "../Component/BlogEdit";
 
-function EditBlog() {
-  return (
-    <div>EditBlog</div>
-  )
+const routeConfig = {
+  Dashboard,
+  "Edit Blog": BlogEdit,
+  "Profile Settings": ProfilePage,
+  News: AdminNews,
+  Sports: AdminSports,
+  Business: AdminBusiness,
+  Entertainment: AdminEntertainment,
+};
+
+function Admin() {
+  const [selectedItem, setSelectedItem] = useState("Edit Blog");
+  const [authenticated, setAuthenticated] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      setAuthenticated(false);
+      window.location.href = "/login";
+    }
+  }, []);
+
+  const handleSidebarItemClick = (item) => {
+    setSelectedItem(item);
+    setOpen(false);
+  };
+
+  const handleSideBar = () => {
+    setOpen(!open);
+  };
+
+  return authenticated ? (
+    <div className="w-full overflow-x-hidden flex items-center">
+      <div className="relative">
+        <AdminNavbar
+          onItemClick={handleSidebarItemClick}
+          onclick={handleSideBar}
+        />
+      </div>
+
+      <div className="flex mt-[80px] w-screen">
+        <div className="md:w-1/6">
+          <AdminSidebar onItemClick={handleSidebarItemClick} open={open} />
+        </div>
+
+        <div className="flex flex-col showcase w-full md:w-5/6">
+          <h5 className="mt-2 text-xl w-full bold font-semibold text-gray-400 -mb-4 px-10 ml-10">
+            {selectedItem}
+          </h5>
+          <div className="px-10 py-5">
+            {React.createElement(routeConfig[selectedItem])}
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Login />
+  );
 }
 
-export default EditBlog
+export default Admin;
