b34d794e528411d7283113cee1d97388ca43ca06
David-mwas <dmwas704@gmaIl.com>
2024-10-08 10:31:48 +0300

    ADD(CORE): react-quill editor, -> html-parser

--- src/Component/homePage/MainNews.jsx
+++ src/Component/homePage/MainNews.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import { Typewriter } from 'react-simple-typewriter';
-import { useTruncate } from '../../../hooks/useTruncate';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
+import { useTruncate } from "../../../hooks/useTruncate";
+import { parse } from "date-fns";
 
-function MainNews (props){
-   
-    const image = props.image;
-    const category= props.category;
-    const title = props.title;
-    
-    const id = props.id
-    const url = `/Blog/${category}/${id}`;
-    const published_on = props.published_on;
-    const slug = useTruncate(props.slug, 200)
+function MainNews(props) {
+  const image = props.image;
+  const category = props.category;
+  const title = props.title;
+  const [parse, setParse] = useState(null); // State to hold the parser function
+  const id = props.id;
+  const url = `/Blog/${category}/${id}`;
+  const published_on = props.published_on;
+  const slug = useTruncate(props.slug, 200);
 
+  useEffect(() => {
+    const loadParser = async () => {
+      const { default: parser } = await import("html-react-parser");
+      setParse(() => parser); // Set the parser function in state
+    };
+
+    loadParser();
+  }, []);
   return (
     <Link to={url}>
       <div className="group hover:scale-105 transition-all duration-200 ease-in-out grid lg:grid-cols-2 lg:gap-4 bg-slate-100 hover:bg-slate-100     rounded-lg  mt-12 p-3">
@@ -42,20 +50,22 @@ function MainNews (props){
           }}
           className="flex flex-col"
         >
-          <div className='flex-1'>
-            
-              <h1 className="font-bold text-2xl group-hover:text-purple-700 w-full  flex r cursor-pointer decoration-gray-400 justify-left  ">
-                {title}
-              </h1>
-            
+          <div className="flex-1">
+            <h1 className="font-bold text-2xl group-hover:text-purple-700 w-full  flex r cursor-pointer decoration-gray-400 justify-left  ">
+              {title}
+            </h1>
+
             <div className="border-none  px-1 text-black ">
-              <Typewriter
-                words={[`${slug}`]}
-                loop={1}
-                delaySpeed={500}
-                cursorBlinking={false}
-                typeSpeed={10}
-              />
+              {
+                parse && <p>{parse(slug)}</p>
+                // <Typewriter
+                //   words={[`${parse(slug)}`]}
+                //   loop={1}
+                //   delaySpeed={500}
+                //   cursorBlinking={false}
+                //   typeSpeed={10}
+                // />
+              }
             </div>
           </div>
           <h1 className="font-light text-[12px]  text-right -bottom-12 pr-3">
@@ -73,4 +83,4 @@ function MainNews (props){
   );
 }
 
-export default MainNews
+export default MainNews;
--- src/Pages/CreateBlog.jsx
+++ src/Pages/CreateBlog.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useRef } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useFormData } from "../../hooks/useFormData";
+import ReactQuill from "react-quill"; // Import ReactQuill
+import "react-quill/dist/quill.snow.css"; // Import Quill styles
 
 const CreateBlog = () => {
   const navigate = useNavigate();
@@ -13,6 +14,20 @@ const CreateBlog = () => {
   useEffect(() => {
     checkUserAuthentication();
   }, []);
+
+  const quillModules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike"],
+      [{ align: [] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["link", "image", "video"],
+      [{ color: [] }, { background: [] }],
+      [{ "code-block": true }],
+      ["clean"],
+    ],
+  };
 
   const checkUserAuthentication = async () => {
     try {
@@ -36,14 +51,14 @@ const CreateBlog = () => {
     } catch (error) {
       console.error("An error occurred while checking authentication:", error);
       setIsAuthenticated(false);
-      if (error == "Error: HTTP error! Status: 401") {
+      if (error === "Error: HTTP error! Status: 401") {
         window.location.href = "/login";
       }
     }
   };
 
   const handleSubmit = async (event) => {
-    const notification = toast.loading("posting your blog...");
+    const notification = toast.loading("Posting your blog...");
     event.preventDefault();
 
     if (!isAuthenticated) {
@@ -56,8 +71,6 @@ const CreateBlog = () => {
 
       const response = await fetch(
         "https://mmust-jowa.onrender.com/api/v1/admin/createblog",
-        // "https://9ef2-197-248-176-94.ngrok-free.app/api/v1/admin/createblog",
-
         {
           method: "POST",
           headers: {
@@ -87,9 +100,6 @@ const CreateBlog = () => {
         console.error("Failed to create blog post");
       }
     } catch (error) {
-      // if () {
-
-      // }
       toast.error("An error occurred", {
         id: notification,
       });
@@ -108,30 +118,30 @@ const CreateBlog = () => {
       };
 
       reader.readAsDataURL(files[0]);
+    } else {
+      updateFormData({ [name]: value });
     }
+  };
 
-    updateFormData({ [name]: type === "file" ? files[0] : value });
+  const handleQuillChange = (name) => (value) => {
+    updateFormData({ [name]: value });
   };
 
   return (
     <>
       <Toaster />
       <div className="w-full">
-        {/* create new blog */}
         <form
-          className="w-full flex items-center justify-center flex-col  mt-2 gap-9  md:grid md:grid-cols-3 max-w-2l bg-white-100  px-4 py-10 mb-10 md:mx-auto sm:text-left  md:mb-12 "
+          className="w-full flex items-center justify-center flex-col mt-2 gap-9 md:grid md:grid-cols-3 max-w-2l bg-white-100 px-4 py-10 mb-10 md:mx-auto sm:text-left md:mb-12"
           onSubmit={handleSubmit}
         >
-          {/* ... your existing form fields ... */}
-
-          {/* posttitle  */}
-          <div className="col-span-2 shadow-lg px-5 w-8/10 w-full ">
-            <div className="mt-2  ">
+          <div className="col-span-2 shadow-lg px-5 w-8/10 w-full">
+            <div className="mt-2">
               <div className="w-full">
-                <label className="block  text-lg  font-semibold text-gray-600">
+                <label className="block text-lg font-semibold text-gray-600">
                   HEADLINE
                 </label>
-                <div className="mt-1 w-[90%] ">
+                <div className="mt-1 w-[90%]">
                   <div className="flex shadow-sm w-full">
                     <input
                       type="text"
@@ -140,49 +150,42 @@ const CreateBlog = () => {
                       onChange={handleChange}
                       value={formData.title}
                       required={true}
-                      className="block  text-2xl font-bold uppercase w-full border-2 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 placeholder:normal-case placeholder:font-normal  sm:text-sm sm:leading-6 rounded-md"
+                      className="block text-2xl font-bold uppercase w-full border-2 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 placeholder:normal-case placeholder:font-normal sm:text-sm sm:leading-6 rounded-md"
                       placeholder="Enter Title"
                     />
                   </div>
                 </div>
               </div>
             </div>
-            <div className=" w-full">
+            <div className="w-full">
               <label className="block mt-2 mb-2 text-lg bold font-semibold text-gray-600">
-                DESCRIPTION
+                SMALL DESCRIPTION
               </label>
-              <div className="mt-1 w-[90%]">
-                <div className="outline-none flex shadow-sm  border-1  w-full">
-                  <input
-                    type="text"
-                    name="slug"
-                    id="slug"
-                    onChange={handleChange}
-                    value={formData.slug}
-                    required={true}
-                    className="block  text-2xl font-bold uppercase w-full border-2 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 placeholder:normal-case placeholder:font-normal  sm:text-sm sm:leading-6 rounded-md"
-                    placeholder="Enter description"
-                  />
-                </div>
+              <div className="mt-2 w-[90%]">
+                <ReactQuill
+                  value={formData.slug}
+                  onChange={handleQuillChange("slug")}
+                  required={true}
+                  className="react-quill"
+                  modules={quillModules}
+                  theme="snow"
+                />
               </div>
             </div>
 
-            {/* post type  */}
-
-            {/* description */}
+            {/* React Quill for DESCRIPTION TEXT */}
             <div className="">
               <label className="block mt-2 mb-2 text-lg bold font-semibold text-gray-600">
-                TEXT
+                DESCRIPTION TEXT
               </label>
               <div className="mt-2 w-[90%]">
-                <textarea
-                  id="about"
-                  name="body"
-                  rows={4}
-                  onChange={handleChange}
+                <ReactQuill
                   value={formData.body}
+                  onChange={handleQuillChange("body")}
                   required={true}
-                  className="resize-none border-2 block w-full h-fixed rounded-md py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"
+                  className="react-quill"
+                  modules={quillModules}
+                  theme="snow"
                 />
               </div>
             </div>
@@ -213,7 +216,7 @@ const CreateBlog = () => {
               </div>
             </div>
 
-            {/* file uploads */}
+            {/* File uploads */}
             <div className="col-span-full">
               <label className="block mb-2 mt-5 text-base text-gray-700">
                 Image
@@ -230,7 +233,7 @@ const CreateBlog = () => {
                   <div className="mt-4 flex text-sm leading-6 text-gray-600">
                     <label
                       htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 w-[110px] h-[30px] "
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 w-[110px] h-[30px]"
                     >
                       <span>Upload a file</span>
                       <input
@@ -239,7 +242,6 @@ const CreateBlog = () => {
                         name="image"
                         type="file"
                         required={true}
-                        // value={formData.image_id}
                         className="sr-only"
                       />
                     </label>
